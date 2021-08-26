094b32b7752f03fb8ae49b9bf43df72b66143906
der mustafa <sihlider27@gmail.com>
2021-08-26 08:38:57 +0300

    Proje dosyası ekle.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Bir class'a public yaptığın zaman o class'a diğer katmanlarda ulaşabilsin demek.
+    //internal class da sadece entities o class 'a erişsin demektir.
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
